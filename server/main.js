e4e38153daa5834bcde14f67bbc24e663820b9f0
MiltonPM <milton.mendoza@processmaker.com>
2018-10-03 16:36:35 -0400

    style 3th

--- server/main.js
+++ server/main.js
@@ -71,6 +71,26 @@ io.on('connection', function(socket,){
     });
 });
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://mongodb.milmeng.svc:27017';
+
+// Database Name
+const dbName = 'sampledb';
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
+
+
 server.listen('8080', function(){
     console.log("Server init test...");
 });

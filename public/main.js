68ae0bbe37be6206d5d833fa3325f19898949252
MiltonPM <milton.mendoza@processmaker.com>
2018-10-03 14:10:36 -0400

    style first

--- public/main.js
+++ public/main.js
@@ -15,7 +15,7 @@ socket.on('display-dice', function(data){
 
 function render(data){
     var html = data.map(function(elem, index){
-        return ('<div><strong>' + elem.author + '</strong>: ' + elem.text + '</div>');
+        return ('<tr><th>' + elem.author + '</th>:<td>' + elem.text + '</td></tr>');
     }).join(" ");  
     document.getElementById('messages').innerHTML = html;
 }
